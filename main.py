55943ea75a3a2bbe9af5bf1045cda004937c9025
Slawek <slawek.ziel0401@gmail.com>
2021-01-30 00:20:07 +0100

    refactor

--- main.py
+++ main.py
@@ -17,20 +17,6 @@ def clientEventCallback(event):
     global data
     data[singleData.device].append(singleData)
 
-if __name__ == "__main234__":
-    d1 = {'d': {'timestamp': 1611878326773, 'temperature': {'inside': '22.89', 'outside': '7.23'}, 'air_pressure': '1015', 'humidity': '38.90', 'deviceId': 'SIR_device01'}}
-    d2 = {'d': {'timestamp': 1611878346773, 'temperature': {'inside': '26.92', 'outside': '9.36'}, 'air_pressure': '1019', 'humidity': '38.15', 'deviceId': 'SIR_device02'}}
-    d3 = {'d': {'timestamp': 1611878366778, 'temperature': {'inside': '24.32', 'outside': '7.69'}, 'air_pressure': '1012', 'humidity': '38.27', 'deviceId': 'SIR_device01'}}
-    d4 = {'d': {'timestamp': 1611878386778, 'temperature': {'inside': '22.42', 'outside': '8.53'}, 'air_pressure': '1013', 'humidity': '38.64', 'deviceId': 'SIR_device02'}}
-    ddd = [d1,d2,d3,d4]
-    data
-    for i in ddd:
-        singleData = dataHandler.dataObject(i)
-        data[singleData.device].append(singleData)
-
-    dataHandler.printData(data, devices)
-    dataHandler.bokehShow(data, devices)
-
 if __name__ == "__main__":
     client = wiotp.sdk.application.ApplicationClient(config=config.getConfig(), logHandlers=None)
     client.connect()
